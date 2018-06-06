9350e7a2fc62959439d13c22bad8582d90693fc5
raoxj <raoxiaojia@gmail.com>
2018-06-06 18:04:42 +0100

    notes

--- Class/Class.js
+++ Class/Class.js
@@ -17,17 +17,17 @@ const levelBall_5 = document.querySelector('.level_5');
 const l2p = 200;
 const l3p = 350;
 const l4p = 500;
-const l5p = 775;
+const l5p = 770;
 const l6p = 2000;
 
 const la1a = 0.95;
-const la2a = 0.75;
-const la3a = 0.15;
+const la2a = 0.7;
+const la3a = 0.1;
 const la4a = 0;
 const la5a = 0;
 const la1b = 0.05;
-const la2b = 0.25;
-const la3b = 0.7;
+const la2b = 0.3;
+const la3b = 0.8;
 const la4b = 0.3;
 const la5b = 0.05;
 
@@ -42,10 +42,14 @@ const la4s = 9;
 const la5s = 5;
 
 const sa1s = 9;
-const sa2s = 7;
+const sa2s = 6;
 const sa3s = 3;
 
 const catam = 120;
+
+const purenames=["Algebraic Geometry","Algebraic Topology","Automata and Formal Languages","Coding and Cryptography","Differential Geometry","Galois Theory","Graph Theory","Linear Analysis","Logic and Set Theory","Number Fields","Number Theory","Optimization and Control","Probability and Measure","Representation Theory","Riemann Surfaces","Topics in Analysis"];
+const appliednames=["Applications of Quantum Mechanics","Applied Probability","Asymptotic Methods","Dynamical Systems","Further Complex Methods","Integrable Systems","Mathematical Biology","Numerical Analysis","Principles of Quantum Mechanics","Principle of Statistics","Statistical Modelling","Stochastic Financial Models"];
+const physicsnames=["Classical Dynamics","Cosmology","Electrodynamics","Fluid Dynamics","General Relativity","Statistical Physics","Waves"];
 
 let level = 1;
 let levelPoints = [0,0,0,0,0,0]; 
@@ -206,24 +210,441 @@ function l20()
 	else
 	if (m2 < l2p)
 	{
-		alertmessage = "距离3/2.2/2.1/1还需"+((l2p-m2)/35).toFixed(2)+"/"+((l3p-m2)/35).toFixed(2)+"/"+((l4p-m2)/35).toFixed(2)+"/"+((l5p-m1)/48).toFixed(2)+"个alpha。一个beta约等于0.45/0.3个alpha(对2.1及以下/first而言)。";
+		alertmessage = "距离3/2.2/2.1/1还需"+((l2p-m2)/35).toFixed(2)+"/"+((l3p-m2)/35).toFixed(2)+"/"+((l4p-m2)/35).toFixed(2)+"/"+((l5p-m1)/50).toFixed(2)+"个alpha。一个beta约等于0.45/0.3个alpha(对2.1及以下/first而言)。";
 		window.alert(alertmessage);
 	}
 	else if (m2<l3p)
 	{
-		alertmessage = "距离2.2/2.1/1还需"+((l3p-m2)/35).toFixed(2)+"/"+((l4p-m2)/35).toFixed(2)+"/"+((l5p-m1)/48).toFixed(2)+"个alpha。一个beta约等于0.45/0.3个alpha(对2.1及以下/first而言)。";
+		alertmessage = "距离2.2/2.1/1还需"+((l3p-m2)/35).toFixed(2)+"/"+((l4p-m2)/35).toFixed(2)+"/"+((l5p-m1)/50).toFixed(2)+"个alpha。一个beta约等于0.45/0.3个alpha(对2.1及以下/first而言)。";
 		window.alert(alertmessage);
 	}
 	else if (m2<l4p)
 	{
-		alertmessage = "距离2.1/1还需"+((l4p-m2)/35).toFixed(2)+"/"+((l5p-m1)/48).toFixed(2)+"个alpha。一个beta约等于0.45/0.3个alpha(对2.1及以下/first而言)。";
+		alertmessage = "距离2.1/1还需"+((l4p-m2)/35).toFixed(2)+"/"+((l5p-m1)/50).toFixed(2)+"个alpha。一个beta约等于0.45/0.3个alpha(对2.1及以下/first而言)。";
 		window.alert(alertmessage);
 	}
 	else
 	{
-		alertmessage = "距离1还需"+"/"+((l5p-m1)/48).toFixed(2)+"个alpha。一个beta约等于0.3个alpha。";
+		alertmessage = "距离1还需"+"/"+((l5p-m1)/50).toFixed(2)+"个alpha。一个beta约等于0.3个alpha。";
 		window.alert(alertmessage);
 	}
+}
+
+function l21()
+{
+		var i;
+		let c1 = 0;
+		let c21 = 0;
+		let c22 = 0;
+		let c3 = 0;
+		let cf = 0;
+		let j=0;
+		for (i=0;i<1000;i++)
+		{
+			let calpha = 0;
+			let cbeta = 0;
+			let craw = 0;
+			for (j=0;j<l1a;j++)
+			{
+					let rnd = Math.random();
+					if (rnd <= la1a)
+					{
+						calpha ++;
+						craw += Math.floor(Math.random()*6)+15;
+					}
+					else if (rnd<=la1a+la1b)
+					{
+						cbeta ++;
+						craw += Math.floor(Math.random()*5)+10;
+					}
+					else
+					{
+						craw += Math.floor(Math.random()*10);
+					}
+			}
+			for (j=0;j<l2a;j++)
+			{
+					let rnd = Math.random();
+					if (rnd <= la2a)
+					{
+						calpha ++;
+						craw += Math.floor(Math.random()*6)+15;
+					}
+					else if (rnd<=la2a+la2b)
+					{
+						cbeta ++;
+						craw += Math.floor(Math.random()*5)+10;
+					}
+					else
+					{
+						craw += Math.floor(Math.random()*10);
+					}
+			}
+			for (j=0;j<l3a;j++)
+			{
+					let rnd = Math.random();
+					if (rnd <= la3a)
+					{
+						calpha ++;
+						craw += Math.floor(Math.random()*6)+15;
+					}
+					else if (rnd<=la3a+la3b)
+					{
+						cbeta ++;
+						craw += Math.floor(Math.random()*5)+10;
+					}
+					else
+					{
+						craw += Math.floor(Math.random()*10);
+					}
+			}
+			for (j=0;j<l4a;j++)
+			{
+					let rnd = Math.random();
+					if (rnd <= la4a)
+					{
+						calpha ++;
+						craw += Math.floor(Math.random()*6)+15;
+					}
+					else if (rnd<=la4a+la4b)
+					{
+						cbeta ++;
+						craw += Math.floor(Math.random()*5)+10;
+					}
+					else
+					{
+						craw += Math.floor(Math.random()*10);
+					}
+			}
+			for (j=0;j<l5a;j++)
+			{
+					let rnd = Math.random();
+					if (rnd <= la5a)
+					{
+						calpha ++;
+						craw += Math.floor(Math.random()*6)+15;
+					}
+					else if (rnd<=la5a+la5b)
+					{
+						cbeta ++;
+						craw += Math.floor(Math.random()*5)+10;
+					}
+					else
+					{
+						craw += Math.floor(Math.random()*10);
+					}
+			}
+			for (j=0;j<s1a;j++)
+			{
+					let rnd = Math.random();
+					if (rnd<=sa1b)
+					{
+						cbeta ++;
+						craw += Math.floor(Math.random()*3)+7;
+					}
+					else
+					{
+						craw += Math.floor(Math.random()*7);
+					}
+			}
+			for (j=0;j<s2a;j++)
+			{
+					let rnd = Math.random();
+					if (rnd<=sa2b)
+					{
+						cbeta ++;
+						craw += Math.floor(Math.random()*3)+7;
+					}
+					else
+					{
+						craw += Math.floor(Math.random()*7);
+					}
+			}
+			for (j=0;j<s3a;j++)
+			{
+					let rnd = Math.random();
+					if (rnd<=sa3b)
+					{
+						cbeta ++;
+						craw += Math.floor(Math.random()*3)+7;
+					}
+					else
+					{
+						craw += Math.floor(Math.random()*7);
+					}
+			}
+			let m2 = calpha*15+cbeta*5+craw+ca*catam;
+			let m1 = m2;
+			if (calpha >= 8) m1 = m2+(calpha-8)*15;
+			if (m1>l5p)
+			{
+					c1 ++;
+			}
+			else if (m2>l4p)
+			{
+					c21 ++;
+			}
+			else if (m2 > l3p)
+			{
+					c22 ++;
+			}
+			else if (m2 > l2p)
+			{
+					c3 ++;
+			}
+			else cf ++;
+		}
+		alertmessage = "1:"+(c1/10).toFixed(1)+"%;\n2.1:"+(c21/10).toFixed(1)+"%;\n2.2:"+(c22/10).toFixed(1)+"%;\n3:"+(c3/10).toFixed(1)+"%;\nF:"+(cf/10).toFixed(1)+"%.";
+		window.alert(alertmessage);
+}
+
+function l22()
+{
+		document.getElementById("main_content").style.display="none";
+		document.getElementById("report_generator").style.display="block";
+}
+
+function l23()
+{
+		document.getElementById("report_generator").style.display="none";
+		document.getElementById("report_div").style.display="block";
+		let report = "University of CMS<br>Mathematical Tripos Part II Examination Results, June 2018<br>"+document.getElementById("name").value+"   "+document.getElementById("college").value + "    ";
+		let coursenames=[];
+		if (document.getElementById("pure").checked) coursenames = coursenames.concat(purenames);
+		if (document.getElementById("applied").checked) coursenames = coursenames.concat(appliednames);
+		if (document.getElementById("physics").checked) coursenames = coursenames.concat(physicsnames);
+		let maxlen = 0;
+		for (i=0;i<coursenames.length;i++)
+		{
+				if (coursenames[i].length > maxlen) maxlen = coursenames[i].length;
+		}
+		maxlen += 3;
+		for (i=0;i<coursenames.length;i++)
+		{
+				while (coursenames[i].length<maxlen) coursenames[i] += ' ';
+		}
+		let details="";
+		let j=0;
+		let c1 = 0;
+		let c21 = 0;
+		let c22 = 0;
+		let c3 = 0;
+		let cf = 0;
+
+			let calpha = 0;
+			let cbeta = 0;
+			let craw = 0;
+			for (j=0;j<l1a;j++)
+			{
+					let rnd = Math.random();
+					if (rnd <= la1a)
+					{
+						calpha ++;
+						araw = Math.floor(Math.random()*6)+15;
+						craw += araw;
+						details = details + coursenames[Math.floor(Math.random()*coursenames.length)] + "    Paper "+(Math.floor(Math.random()*4)+1)+"      II Q"+(Math.floor(Math.random()*30)+11)+"   "+araw+"<br>";
+					}
+					else if (rnd<=la1a+la1b)
+					{
+						cbeta ++;
+						araw = Math.floor(Math.random()*5)+10;
+						craw += araw;
+						details = details + coursenames[Math.floor(Math.random()*coursenames.length)] + "    Paper "+(Math.floor(Math.random()*4)+1)+"      II Q"+(Math.floor(Math.random()*30)+11)+"   "+araw+"<br>";
+					}
+					else
+					{
+						araw = Math.floor(Math.random()*10);
+						craw += araw;
+						details = details + coursenames[Math.floor(Math.random()*coursenames.length)] + "    Paper "+(Math.floor(Math.random()*4)+1)+"      II Q"+(Math.floor(Math.random()*30)+11)+"   "+araw+"<br>";
+					}
+			}
+			for (j=0;j<l2a;j++)
+			{
+					let rnd = Math.random();
+					if (rnd <= la2a)
+					{
+						calpha ++;
+						araw = Math.floor(Math.random()*6)+15;
+						craw += araw;
+						details = details + coursenames[Math.floor(Math.random()*coursenames.length)] + "    Paper "+(Math.floor(Math.random()*4)+1)+"      II Q"+(Math.floor(Math.random()*30)+11)+"   "+araw+"<br>";
+					}
+					else if (rnd<=la2a+la2b)
+					{
+						cbeta ++;
+						araw = Math.floor(Math.random()*5)+10;
+						craw += araw;
+						details = details + coursenames[Math.floor(Math.random()*coursenames.length)] + "    Paper "+(Math.floor(Math.random()*4)+1)+"      II Q"+(Math.floor(Math.random()*30)+11)+"   "+araw+"<br>";
+					}
+					else
+					{
+						araw = Math.floor(Math.random()*10);
+						craw += araw;
+						details = details + coursenames[Math.floor(Math.random()*coursenames.length)] + "    Paper "+(Math.floor(Math.random()*4)+1)+"      II Q"+(Math.floor(Math.random()*30)+11)+"   "+araw+"<br>";
+					}
+			}
+			for (j=0;j<l3a;j++)
+			{
+					let rnd = Math.random();
+					if (rnd <= la3a)
+					{
+						calpha ++;
+						araw = Math.floor(Math.random()*6)+15;
+						craw += araw;
+						details = details + coursenames[Math.floor(Math.random()*coursenames.length)] + "    Paper "+(Math.floor(Math.random()*4)+1)+"      II Q"+(Math.floor(Math.random()*30)+11)+"   "+araw+"<br>";
+					}
+					else if (rnd<=la3a+la3b)
+					{
+						cbeta ++;
+						araw = Math.floor(Math.random()*5)+10;
+						craw += araw;
+						details = details + coursenames[Math.floor(Math.random()*coursenames.length)] + "    Paper "+(Math.floor(Math.random()*4)+1)+"      II Q"+(Math.floor(Math.random()*30)+11)+"   "+araw+"<br>";
+					}
+					else
+					{
+						araw = Math.floor(Math.random()*10);
+						craw += araw;
+						details = details + coursenames[Math.floor(Math.random()*coursenames.length)] + "    Paper "+(Math.floor(Math.random()*4)+1)+"      II Q"+(Math.floor(Math.random()*30)+11)+"   "+araw+"<br>";
+					}
+			}
+			for (j=0;j<l4a;j++)
+			{
+					let rnd = Math.random();
+					if (rnd <= la4a)
+					{
+						calpha ++;
+						araw = Math.floor(Math.random()*6)+15;
+						craw += araw;
+						details = details + coursenames[Math.floor(Math.random()*coursenames.length)] + "    Paper "+(Math.floor(Math.random()*4)+1)+"      II Q"+(Math.floor(Math.random()*30)+11)+"   "+araw+"<br>";
+					}
+					else if (rnd<=la4a+la4b)
+					{
+						cbeta ++;
+						araw = Math.floor(Math.random()*5)+10;
+						craw += araw;
+						details = details + coursenames[Math.floor(Math.random()*coursenames.length)] + "    Paper "+(Math.floor(Math.random()*4)+1)+"      II Q"+(Math.floor(Math.random()*30)+11)+"   "+araw+"<br>";
+					}
+					else
+					{
+						araw = Math.floor(Math.random()*10);
+						craw += araw;
+						details = details + coursenames[Math.floor(Math.random()*coursenames.length)] + "    Paper "+(Math.floor(Math.random()*4)+1)+"      II Q"+(Math.floor(Math.random()*30)+11)+"   "+araw+"<br>";
+					}
+			}
+			for (j=0;j<l5a;j++)
+			{
+					let rnd = Math.random();
+					if (rnd <= la5a)
+					{
+						calpha ++;
+						araw = Math.floor(Math.random()*6)+15;
+						craw += araw;
+						details = details + coursenames[Math.floor(Math.random()*coursenames.length)] + "    Paper "+(Math.floor(Math.random()*4)+1)+"      II Q"+(Math.floor(Math.random()*30)+11)+"   "+araw+"<br>";
+					}
+					else if (rnd<=la5a+la5b)
+					{
+						cbeta ++;
+						araw = Math.floor(Math.random()*5)+10;
+						craw += araw;
+						details = details + coursenames[Math.floor(Math.random()*coursenames.length)] + "    Paper "+(Math.floor(Math.random()*4)+1)+"      II Q"+(Math.floor(Math.random()*30)+11)+"   "+araw+"<br>";
+					}
+					else
+					{
+						araw = Math.floor(Math.random()*10);
+						craw += araw;
+						details = details + coursenames[Math.floor(Math.random()*coursenames.length)] + "    Paper "+(Math.floor(Math.random()*4)+1)+"      II Q"+(Math.floor(Math.random()*30)+11)+"   "+araw+"<br>";
+					}
+			}
+			for (j=0;j<s1a;j++)
+			{
+					let rnd = Math.random();
+					if (rnd<=sa1b)
+					{
+						cbeta ++;
+						araw = Math.floor(Math.random()*3)+7;
+						craw += araw;
+						details = details + coursenames[Math.floor(Math.random()*coursenames.length)] + "    Paper "+(Math.floor(Math.random()*4)+1)+"      I Q"+(Math.floor(Math.random()*10)+1)+"   "+araw+"<br>";
+					}
+					else
+					{
+						araw = Math.floor(Math.random()*7);
+						craw += araw;
+						details = details + coursenames[Math.floor(Math.random()*coursenames.length)] + "    Paper "+(Math.floor(Math.random()*4)+1)+"      I Q"+(Math.floor(Math.random()*10)+1)+"   "+araw+"<br>";
+					}
+			}
+			for (j=0;j<s2a;j++)
+			{
+					let rnd = Math.random();
+					if (rnd<=sa2b)
+					{
+						cbeta ++;
+						araw = Math.floor(Math.random()*3)+7;
+						craw += araw;
+						details = details + coursenames[Math.floor(Math.random()*coursenames.length)] + "    Paper "+(Math.floor(Math.random()*4)+1)+"      I Q"+(Math.floor(Math.random()*10)+1)+"   "+araw+"<br>";
+					}
+					else
+					{
+						araw = Math.floor(Math.random()*7);
+						craw += araw;
+						details = details + coursenames[Math.floor(Math.random()*coursenames.length)] + "    Paper "+(Math.floor(Math.random()*4)+1)+"      I Q"+(Math.floor(Math.random()*10)+1)+"   "+araw+"<br>";
+					}
+			}
+			for (j=0;j<s3a;j++)
+			{
+					let rnd = Math.random();
+					if (rnd<=sa3b)
+					{
+						cbeta ++;
+						araw = Math.floor(Math.random()*3)+7;
+						craw += araw;
+						details = details + coursenames[Math.floor(Math.random()*coursenames.length)] + "    Paper "+(Math.floor(Math.random()*4)+1)+"      I Q"+(Math.floor(Math.random()*10)+1)+"   "+araw+"<br>";
+					}
+					else
+					{
+						araw = Math.floor(Math.random()*7);
+						craw += araw;
+						details = details + coursenames[Math.floor(Math.random()*coursenames.length)] + "    Paper "+(Math.floor(Math.random()*4)+1)+"      I Q"+(Math.floor(Math.random()*10)+1)+"   "+araw+"<br>";
+					}
+			}
+			let m2 = calpha*15+cbeta*5+craw+ca*catam;
+			let m1 = m2;
+			if (calpha >= 8) m1 = m2+(calpha-8)*15;
+			let tm = 0;
+			if (m1>l5p)
+			{
+					report = report + "Class I<br>";
+					tm = (m1-l5p)/(l6p-l5p)*25+70;
+			}
+			else if (m2>l4p)
+			{
+					report = report + "Class II.1<br><br>";
+					tm = (m2-l4p)/(l5p-l4p)*10+60;
+			}
+			else if (m2 > l3p)
+			{
+					report = report + "Class II.2<br><br>";
+					tm = (m2-l3p)/(l4p-l3p)*10+50;
+			}
+			else if (m2 > l2p)
+			{
+					report = report + "Class III<br><br>";
+					tm = (m2-l2p)/(l3p-l2p)*10+40;
+			}
+			else 
+			{
+					report = report + "Failed<br>";
+					tm = (m2-0)/(l2p-l1p)*40+0;
+			}
+		report = report + "Total mark, alpha, beta = "+craw+", "+calpha+", "+cbeta+"   Merit = "+m1+" Transcript mark = "+tm.toFixed(0)+"%<br>"+details+"<br>Computational Projects (scaled total)  ";
+		if (ca == 1) report += "120<br>";
+		else report += "0<br>";
+		report = "<pre>"+report+"</pre>";
+		document.getElementById("report_content").innerHTML=report;
+}
+
+function l24()
+{
+		document.getElementById("report_div").style.display="none";
+		document.getElementById("main_content").style.display="block";
 }
 
 function tween() {
